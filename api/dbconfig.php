1963381205062cc2c83a7941e58da9848eb92062
Aglitoiu Marius <aglitoiu@Aglitoius-MacBook-Pro.local>
2019-12-14 12:17:22 +0200

    API Work

---
+++ api/dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "macos";
+$db = "smartWheels";
+$conn = new mysqli($servername, $username, $password,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ 
+ 
+
+
+?>
--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
-
+require_once("dbconfig.php");
+if($_GET['req']=="fetchroutes"&&$_GET['id']==6){
+    $query="SELECT * from Route_6";
+    $result=mysqli_query($conn,$query);
+    while($row=mysqli_fetch_assoc($result)){
+        $emparray[] = $row;
+    }
+    echo json_encode($emparray);
+}
 
 ?>
